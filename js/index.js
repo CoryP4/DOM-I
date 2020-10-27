36882267c6dfcde08fd28194ab6cee736b897037
coryp4 <cory@burtano.com>
2020-10-27 07:41:06 -0700

    mostly done

--- js/index.js
+++ js/index.js
@@ -41,8 +41,50 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
+//circle code snippet
 
 const codeImg = document.querySelector("#cta-img")
 codeImg.setAttribute('src', "img/header-img.png")
 
-//notes notes notes
+//Nav bar 
+
+const links1 = document.querySelector("header nav a:nth-of-type(1)")
+links1.textContent = "Services"
+
+const links2 = document.querySelector("header nav a:nth-of-type(2)")
+links2.textContent = "Product"
+
+const links3 = document.querySelector("header nav a:nth-of-type(3)")
+links3.textContent = "Vision"
+
+const links4 = document.querySelector("header nav a:nth-of-type(4)")
+links4.textContent = "Features"
+
+const links5 = document.querySelector("header nav a:nth-of-type(5)")
+links5.textContent = "About"
+
+const links6 = document.querySelector("header nav a:nth-of-type(6)")
+links6.textContent = "Contact"
+
+
+//bold text heading & button
+
+const ctaText = document.querySelector(".cta-text h1")
+ctaText.innerHTML = `DOM<br> Is<br> Awesome`
+
+const ctaButton = document.querySelector(".cta-text button")
+ctaButton.textContent = "Get Started"
+
+//top content 
+
+const topText1 = document.querySelector(".top-content .text-content:nth-of-type(1) h4")
+topText1.textContent = "Features"
+
+const topPara1 = document.querySelector(".top-content .text-content:nth-of-type(1) p")
+topPara1.textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+const topText2 = document.querySelector(".top-content .text-content:nth-of-type(2) h4")
+topText2.textContent = "About"
+
+const topPara2 = document.querySelector(".top-content .text-content:nth-of-type(2) p")
+topPara2.textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
